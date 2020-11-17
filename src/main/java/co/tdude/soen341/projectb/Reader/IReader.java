7debdd1674f6e6daabe57bd397e8fc234e6ba8d8
David <dav.molina18@gmail.com>
2020-11-17 14:16:44 -0500

    Added Reader class, and IReader interface to read .asm files.

---
+++ src/main/java/co/tdude/soen341/projectb/Reader/IReader.java
@@ -0,0 +1,11 @@
+package co.tdude.soen341.projectb.Reader;
+
+import java.io.IOException;
+
+/**
+ * Interface IReader
+ * @author David Molina (40111257)
+ */
+public interface IReader {
+    char read() throws IOException;
+}
