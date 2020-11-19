0c3b309a12946be4012bf9c88d1db33612020172
Torious <46064671+Torious@users.noreply.github.com>
2020-11-18 19:11:44 -0500

    added SourceFile class for Storing the assembly file (#20)
    * added SourceFile class for Storing the assembly file
    
    * Fixed compilation issue due to SourceFile class. #20
    
    Co-authored-by: Karim <karim.rhoualem@gmail.com>

---
+++ src/main/java/co/tdude/soen341/projectb/Assembler/SourceFile.java
@@ -0,0 +1,22 @@
+package co.tdude.soen341.projectb.Assembler;
+
+import java.io.File;
+
+public final class SourceFile {
+
+	private static File _asmFile;
+	private static String _asmFileStr;
+	
+	public static void StoreAssemblyFile(File asmFile) {
+		
+		_asmFile = asmFile;
+	
+	}
+	
+	public static void ConvertToString() {
+		
+		_asmFileStr = _asmFile.toString();
+		
+	}
+
+}
