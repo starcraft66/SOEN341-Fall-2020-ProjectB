5cfdb338199b1381320a6ea116221edd213cc916
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-11-25 22:07:24 -0500

    Added end to end tests

---
+++ src/test/resources/asm_input.asm
@@ -0,0 +1,28 @@
+ halt
+ pop
+ dup
+ exit
+ ldv.u3 4
+ ret
+ not
+ and
+ or
+ br.i5 10
+ xor
+ neg
+ inc
+ dec
+ addv.u3 2
+ sub
+ mul
+ div
+ rem
+ shl
+ shr
+ teq
+ tne
+ tlt
+ tgt
+ tle
+ tge
+ halt
